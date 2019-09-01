d96db3b0f2b03eadc94c863de5718207cd4c9aaa
FoxGeoff <fox.geoffrey@gmail.com>
2019-09-01 15:55:56 -0700

    Add initialize a new UserManager

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,19 +6,25 @@ import { UserManager, UserManagerSettings, User } from 'oidc-client';
 })
 export class AuthService {
 
-  constructor() { }
+  private manager = new UserManager(getClientSettings());
+  private user: User = null;
 
+  constructor() {
+    this.manager.getUser().then(user => {
+      this.user = user;
+    });
+  }
 }
 
 export function getClientSettings(): UserManagerSettings {
   return {
-      authority: 'http://localhost:5555/',
-      client_id: 'angular_spa',
-      redirect_uri: 'http://localhost:4200/auth-callback',
-      post_logout_redirect_uri: 'http://localhost:4200/',
-      response_type:"id_token token",
-      scope:"openid profile api1",
-      filterProtocolClaims: true,
-      loadUserInfo: true
+    authority: 'http://localhost:5555/',
+    client_id: 'angular_spa',
+    redirect_uri: 'http://localhost:4200/auth-callback',
+    post_logout_redirect_uri: 'http://localhost:4200/',
+    response_type: "id_token token",
+    scope: "openid profile api1",
+    filterProtocolClaims: true,
+    loadUserInfo: true
   };
 }
